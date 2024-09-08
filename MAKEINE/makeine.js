ccef2fddd66e40a33405e26d382309d558268b8e
Victear <xvictearx@gmail.com>
2024-09-08 14:40:19 -0400

    general clean up of code to start choice trees

--- MAKEINE/makeine.js
+++ MAKEINE/makeine.js
@@ -1,56 +1,63 @@
+// IDS
 let playButton = document.getElementById("playButton")
 let h1 = document.getElementById("playButton2")
 let bottomtext = document.getElementById("bottomtext")
 let firstDisplay = document.getElementsByClassName("firstDisplay")
+
 let komari = document.getElementById("komari")
 let socialId = document.getElementById("socials")
 let socials = document.getElementsByClassName("socials")
 
-let jumpCount = 0
-// let body = document.getElementsByTagName("body")
 let annaNorm = document.getElementById("annaNorm")
-
 let hello = document.getElementById("hello")
 
+// let body = document.getElementsByTagName("body")
+
+
+// AUDIO
 let startAudio = new Audio("makeineop.mp3")
 let clickAudio = new Audio("smash.wav")
 let clickB = new Audio("button2.mp3")
 let kill = new Audio("bwaan.mp3")
 
-// clickB.currentTime = 0.000005
-
 let canny1Audio = new Audio("canny1.mp3")
 let canny2Audio = new Audio("canny2.mp3")
 let canny3Audio = new Audio("canny3.mp3")
 let cannyAudio = new Audio("canny.mp3")
-clickAudio.volume = 0.2
 
-//  canny1Audio.currentTime= 0.1
-//  canny2Audio.currentTime= 0.1
-//  canny3Audio.currentTime= 0.1
-//  cannyAudio.currentTime= 0.1
-    
+
+// INITIAL AUDIO SETTINGS
+clickAudio.volume = 0.3
+// clickB.currentTime = 0.000005
+
+
+// GLOBAL CONSTANTS
+let CALORIE_COUNT = 0
+let STARTER_MONEY = 10
+
+let currentMoney = 0
+
+let jumpCount = 0
 let blinkToggle = true
 
 
-// function blink(obj){
-//     if (obj && blinkToggle){
-//         obj.style.display = (obj.style.display == "block" ? "none" : "block")
-//         setTimeout(function(){blink(obj)}, 400)
-//     }
-//     else{
-//         obj.style.display = "block"
-//     }
-    
-    
-// }
 
+
+
+
+// CODE STARTS HERE
 function removeDisplay(){
     bottomtext.style.display = "none"
     playButton.style.display = "none"
     h1.style.display = "none"
-
 }
+
+function hoverSound(){
+    console.log("over")
+    clickB.play()
+    clickB.currentTime = 0
+}
+
 function bounce(){
     annaNorm.style.transform = "translate(-50%, -150%)"
 }
@@ -94,40 +101,34 @@ function start(){
 
         // document.addEventListener("click", canny)
 
-        
-
-        
     }, 900)
     
     setTimeout(function(){
         removeDisplay()
-        
     }, 1600)
+
     setTimeout(function(){annaNorm.classList.add("bounce")}, 1800)
     annaNorm.classList.add("annaNormBounce")
     hello.style.display = "block"
 
-
     annaNorm.addEventListener("click", jumping)
-    
-
-    
-    
 }
 
-function jumping(){
-    
+
+function jumping(){  
     jumpCount += 1
+    // console.log(jumpCount)
 
     if (jumpCount % 5 === 0){
+        jumpCount = 0
         annaNorm.src = "ANNAkill.png"
-        annaNorm.style.filter = `contrast(1.4) brightness(0.9)`
+        annaNorm.style.filter = `contrast(1.1) brightness(0.8)`
         
         document.body.style.backgroundColor = "rgb(20, 30, 40)"
-        console.log(kill.duration)
+        // console.log(kill.duration)
         
         let duration = parseFloat(kill.duration) * 1000 + 1000
-        console.log(duration)
+        // console.log(duration)
         kill.play()
         annaNorm.removeEventListener("click", jumping)
         annaNorm.classList.remove("annaNormBounce")
@@ -136,9 +137,7 @@ function jumping(){
         setTimeout(function(){
             annaNorm.addEventListener("click", jumping)
             annaNorm.classList.add("annaNormBounce")
-        }
-            
-            , duration)
+        }, duration)
         
     }
     else{
@@ -155,19 +154,17 @@ function jumping(){
     annaNorm.classList.add("bounceUp");
     annaNorm.classList.add("newAnnaPos");
     annaNorm.classList.remove("bounce");
+
     setTimeout(function(){annaNorm.classList.remove("bounceUp")}
                 ,200)
-                   
-      
-}
-
-function hoverSound(){
-    console.log("over")
-    clickB.play()
-    clickB.currentTime = 0
 }
 
 
+
+
+
+
+// STARTING SCREEN
 setTimeout(
     function(){
         playButton.addEventListener("click", start)
